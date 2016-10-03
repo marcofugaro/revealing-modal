a16f4f0b3d546a1ae729d530a66a0993acd30960
Marco Fugaro <marco.fugaro@gmail.com>
2016-10-03 15:48:23 +0200

    removed any trace of gulp :see_no_evil:

--- tasks/clean.js
+++
@@ -1,9 +0,0 @@
-import gulp from 'gulp';
-import del from 'del';
-
-import config from './../gulpfile.babel';
-
-
-gulp.task('clean', function() {
-  return del([config.buildDir]);
-});
--- tasks/watch.js
+++
@@ -1,9 +0,0 @@
-import gulp from 'gulp';
-
-import config from './../gulpfile.babel';
-
-
-gulp.task('watch', function() {
-  gulp.watch(config.styles.src,  ['styles']);
-  gulp.watch(config.scripts.src, ['eslint']);
-});
